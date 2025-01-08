a90c3756a385cd91e1d4e0c0ee5bd90aacbfc27a
Boris Damevin <borisdamevin@gmail.com>
2025-01-08 10:49:55 +0100

    chore(ci): add stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+// ============================================================================================= //
+//                                           STYLELINT                                           //
+// ============================================================================================= //
+
+export default {
+    extends: [
+        '@front-factory/stylelint-config'
+    ],
+    rules: {
+        'at-rule-empty-line-before': null,
+        'custom-property-pattern': null,
+        'scss/at-if-no-null': null
+    }
+};
